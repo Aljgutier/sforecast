cb47c20f1bb0f7ac6524216fa27b5d27c476c01c
Al Gutierrez <aljgutier@yahoo.com>
2022-09-15 19:09:34 -0700

    feat: 0,1.0 progress

--- src/sforecast/__init__.py
+++ src/sforecast/__init__.py
@@ -1,3 +1,6 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("sforecast")
+
+from .sforecast import sliding_forecast
+from .sforecast import sforecast
