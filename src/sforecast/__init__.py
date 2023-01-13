26f980a24ac94df3d3b26f0ee0b2c60095692a33
Al Gutierrez <aljgutier@yahoo.com>
2023-01-13 13:25:56 -0800

    feat: fix issues with out-of-sample train/test, add forward prediction

--- src/sforecast/__init__.py
+++ src/sforecast/__init__.py
@@ -3,8 +3,9 @@ from importlib.metadata import version
 __version__ = version("sforecast")
 
 from .sforecast import sforecast
-from .sforecast import ci_tdistribution
-from .sforecast import ci_percentile
-from .sforecast import nlag_covars
-from .sforecast import min_func
-from .sforecast import max_func
+#from .sforecast import ci_tdistribution
+#from .sforecast import ci_percentile
+#from .sforecast import nlag_covars
+#from .sforecast import min_func
+#from .sforecast import max_func
+#from .sforecast import minmaxstd_scaler
